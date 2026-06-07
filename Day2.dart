59782a363c1ecc752de0357005d58f1373b58963
Suraj Kumar <surajgenius66678@gmail.com>
2026-06-08 04:30:33 +0530

    Day 2 - Dart practice

---
+++ Day2.dart
@@ -0,0 +1,56 @@
+// // Classes- are blue prints or templates for creating obj 
+// void main(){
+//     final Cook = Cookie(shape :'rect',size:67);
+     
+// //  print("${Cook.size} cm");
+// //  Cook.baking();
+// // final iscookiecooling = Cook.isColling();
+// // print(iscookiecooling);
+// // Cook.shape="rect";
+// // Cook.size=12.4;
+// // Cook.size=7;
+// // print(Cook.shape);
+// // // print(Cook.size);
+// // Cook._height=9;
+// print(Cook.height);// private variable -a variable limited to only 1 file in dart start with"_"
+// // print(Cook.this);
+// Cook.sHeight=10;
+// print(Cook.height);
+// }
+// class Cookie {
+// final String shape;
+// final double size;
+//   Cookie({required this.shape,required this.size}){
+//     print(this);
+//     print("Cookies constructor is called");
+//     baking();
+//    print(isColling());
+//   }
+//   int _height=0;
+
+// // method - a fuction in a class
+// void baking (){
+//     print("Baking has started");
+// } 
+// bool isColling (){
+//     return false;
+// }
+// // getter
+// int get height {
+//     return _height;
+// }
+// // setter
+// set sHeight(int h){
+//     _height=h;
+// }
+// }
+
+
+void(){
+print (   Constants.grreting);
+}
+class Constants{
+    String greeting ='Hello ,How are you ';
+    String bye = 'Bye';
+
+}
